5c990f3aaca98c3dc5dad024bb3c7c435b3169cb
ZPelletierCSUSTAN <zpelletier@csustan.edu>
2025-12-19 00:53:12 -0800

    Initial commit

--- app.py
+++ app.py
@@ -32,7 +32,35 @@ DEFAULT_CATEGORIES = [
 DEFAULT_FEEDS = [
     {'id': 'wn1', 'name': 'CNN', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCupvZG-5ko_eiXAupbDfxWw', 'category': 'World News'},
     {'id': 'wn2', 'name': 'BBC News', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UC16niRr50-MSBwiO3YDb3RA', 'category': 'World News'},
+    {'id': 'wn3', 'name': 'NBC News', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCeY0bbntWzzVIaj2z3QigXg', 'category': 'World News'},
+    {'id': 'wn4', 'name': 'ABC News', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCBi2mrWuNuyYy4gbM6fU18Q', 'category': 'World News'},
     {'id': 'tc1', 'name': 'MKBHD', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCBJycsmduvYEL83R_U4JriQ', 'category': 'Tech'},
+    {'id': 'tc2', 'name': 'Unbox Therapy', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCsTcErHg8oDvUnTzoqsYeNw', 'category': 'Tech'},
+    {'id': 'tc3', 'name': 'Android Authority', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCdOXRB936PKSwx0J7SgF6SQ', 'category': 'Tech'},
+    {'id': 'tc4', 'name': 'IGN Tech', 'url': 'https://www.ign.com/rss/v2/articles/feed?vertical=tech', 'category': 'Tech'},
+    {'id': 'cm1', 'name': 'DC', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UC4kjDjhexSVuC8JWk4ZanFw', 'category': 'Comics'},
+    {'id': 'cm2', 'name': 'IGN Comics', 'url': 'https://www.ign.com/rss/v2/articles/feed?channel=comics', 'category': 'Comics'},
+    {'id': 'tv1', 'name': 'Netflix', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCWOA1ZGywLbqmigxE4Qlvuw', 'category': 'TV & Movies'},
+    {'id': 'tv2', 'name': 'Marvel', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCvC4D8onUfXzvjTOM-dBfEA', 'category': 'TV & Movies'},
+    {'id': 'tv3', 'name': 'Flicks And The City', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCz1GPotHecuLngiLuY739QQ', 'category': 'TV & Movies'},
+    {'id': 'tv4', 'name': 'Rotten Tomatoes', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCQJWtTnAHhEG5w4uN0udnUQ', 'category': 'TV & Movies'},
+    {'id': 'tv5', 'name': 'IGN Movies', 'url': 'https://www.ign.com/rss/v2/articles/feed?channel=movies', 'category': 'TV & Movies'},
+    {'id': 'tv6', 'name': 'IGN TV', 'url': 'https://www.ign.com/rss/v2/articles/feed?channel=tv', 'category': 'TV & Movies'},
+    {'id': 'vg1', 'name': 'IGN Games', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCKy1dAqELo0zrOtPkf0eTMw', 'category': 'Video Games'},
+    {'id': 'vg2', 'name': 'GameSpot', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCbu2SsF-Or3Rsn3NxqODImw', 'category': 'Video Games'},
+    {'id': 'vg3', 'name': 'IGN Xbox', 'url': 'https://www.ign.com/rss/v2/articles/feed?channel=xbox', 'category': 'Video Games'},
+    {'id': 'vg4', 'name': 'IGN Nintendo', 'url': 'https://www.ign.com/rss/v2/articles/feed?channel=nintendo', 'category': 'Video Games'},
+    {'id': 'vg5', 'name': 'IGN PC', 'url': 'https://www.ign.com/rss/v2/articles/feed?channel=pc', 'category': 'Video Games'},
+    {'id': 'vg6', 'name': 'IGN PlayStation', 'url': 'https://www.ign.com/rss/v2/articles/feed?channel=playstation', 'category': 'Video Games'},
+    {'id': 'mu1', 'name': 'Pitchfork', 'url': 'https://pitchfork.com/feed/rss', 'category': 'Music'},
+    {'id': 'mu2', 'name': 'The Needle Drop', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCt7fwAhXDy3oNFTAzF2o8Pw', 'category': 'Music'},
+    {'id': 'mu3', 'name': 'Deep Cuts', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCBv7HEHuVlNAELGi5XJd85Q', 'category': 'Music'},
+    {'id': 'mu4', 'name': 'Rolling Stone', 'url': 'https://www.rollingstone.com/feed/rss/', 'category': 'Music'},
+    {'id': 'fd1', 'name': 'One Bite Pizza', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCRm922a9Bdd58Q1QJpwE9Xg', 'category': 'Food'},
+    {'id': 'fd2', 'name': 'Joshua Weissman', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCXw1ddyrUmib3zmCmvSI1ow', 'category': 'Food'},
+    {'id': 'ot1', 'name': 'BBC Earth', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCwmZiChSryoWQCZMIQezgTg', 'category': 'Other'},
+    {'id': 'ot2', 'name': 'Business Insider', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCYmna5rFHIesFteksAvFOfg', 'category': 'Other'},
+    {'id': 'ot3', 'name': 'Insider Tech', 'url': 'https://www.youtube.com/feeds/videos.xml?channel_id=UCJXxuESjGIaaqb1-IRWFihw', 'category': 'Other'},
 ]
 
 # --- Helpers ---
@@ -58,7 +86,6 @@ def get_user_config(username):
         'fav_categories': ['Read Later', 'Best Of'],
         'favorites': []
     })
-    # Ensure structure
     if 'categories' not in user_data: user_data['categories'] = list(DEFAULT_CATEGORIES)
     if 'feeds' not in user_data: user_data['feeds'] = list(DEFAULT_FEEDS)
     if 'favorites' not in user_data: user_data['favorites'] = []
@@ -116,26 +143,26 @@ def fetch_single_feed(feed):
 
 @app.route('/')
 def index():
+    # Root redirects to Accounts (index.html) if not logged in, or Home (page1.html) if logged in.
+    if 'user' not in session:
+        return redirect(url_for('accounts'))
+    
+    # Check if user actually exists in data
+    all_data = get_all_data()
+    if session['user'] not in all_data:
+        session.clear()
+        return redirect(url_for('accounts'))
+        
+    return redirect(url_for('home'))
+
+@app.route('/accounts', methods=['GET', 'POST'])
+def accounts():
     all_data = get_all_data()
     if not all_data:
         default_user = "Default User"
         all_data[default_user] = {'categories': list(DEFAULT_CATEGORIES), 'feeds': list(DEFAULT_FEEDS), 'favorites': [], 'fav_categories': ['Read Later']}
         save_all_data(all_data)
-        session['user'] = default_user
-        return redirect(url_for('index'))
-
-    if 'user' not in session: return redirect(url_for('accounts'))
-    if session['user'] not in all_data:
-        session.clear()
-        return redirect(url_for('accounts'))
-
-    user_data, _ = get_user_config(session['user'])
-    # Render Page 2 (Home/Index)
-    return render_template('page2.html', categories=user_data['categories'])
-
-@app.route('/accounts', methods=['GET', 'POST'])
-def accounts():
-    all_data = get_all_data()
+        
     if request.method == 'POST':
         action = request.form.get('action')
         if action == 'create':
@@ -144,13 +171,13 @@ def accounts():
                 all_data[username] = {'categories': list(DEFAULT_CATEGORIES), 'feeds': list(DEFAULT_FEEDS)}
                 save_all_data(all_data)
                 session['user'] = username
-                return redirect(url_for('index'))
+                return redirect(url_for('home'))
             else: flash("User already exists.", "error")
         elif action == 'login':
             username = request.form.get('username')
             if username in all_data:
                 session['user'] = username
-                return redirect(url_for('index'))
+                return redirect(url_for('home'))
         elif action == 'delete':
             username = request.form.get('username')
             if username in all_data:
@@ -158,8 +185,16 @@ def accounts():
                 save_all_data(all_data)
                 if session.get('user') == username: session.pop('user', None)
             return redirect(url_for('accounts'))
-    # Render Page 1 (Accounts)
-    return render_template('page1.html', users=list(all_data.keys()))
+            
+    # Render INDEX.HTML (Accounts)
+    return render_template('index.html', users=list(all_data.keys()))
+
+@app.route('/home')
+def home():
+    if 'user' not in session: return redirect(url_for('accounts'))
+    user_data, _ = get_user_config(session['user'])
+    # Render PAGE1.HTML (Home)
+    return render_template('page1.html', categories=user_data['categories'])
 
 @app.route('/logout')
 def logout():
@@ -176,8 +211,8 @@ def show_category(category_name):
         results = executor.map(fetch_single_feed, target_feeds)
         for res in results: articles.extend(res)
     articles.sort(key=lambda x: x['timestamp'], reverse=True)
-    # Render Page 3 (Articles)
-    return render_template('page3.html', category=category_name, articles=articles, feed_count=len(target_feeds), fav_categories=user_data['fav_categories'])
+    # Render PAGE2.HTML (Articles)
+    return render_template('page2.html', category=category_name, articles=articles, feed_count=len(target_feeds), fav_categories=user_data['fav_categories'])
 
 @app.route('/favorites', methods=['GET', 'POST'])
 def favorites():
@@ -200,8 +235,8 @@ def favorites():
         all_data[username] = user_data
         save_all_data(all_data)
         return redirect(url_for('favorites'))
-    # Render Page 4 (Favorites)
-    return render_template('page4.html', favorites=user_data['favorites'], fav_categories=user_data['fav_categories'])
+    # Render PAGE3.HTML (Favorites)
+    return render_template('page3.html', favorites=user_data['favorites'], fav_categories=user_data['fav_categories'])
 
 @app.route('/save_article', methods=['POST'])
 def save_article():
@@ -246,8 +281,8 @@ def dashboard():
         all_data[username] = user_data
         save_all_data(all_data)
         return redirect(url_for('dashboard'))
-    # Render Page 5 (Manager)
-    return render_template('page5.html', categories=user_data['categories'], feeds=user_data['feeds'])
+    # Render PAGE4.HTML (Manager)
+    return render_template('page4.html', categories=user_data['categories'], feeds=user_data['feeds'])
 
 @app.route('/finder', methods=['GET', 'POST'])
 def finder():
@@ -282,18 +317,18 @@ def finder():
                 found_feeds = unique_feeds[:FINDER_RESULT_LIMIT]
                 if not found_feeds: error = "No RSS feeds found."
             except: error = "Could not connect."
-    # Render Page 6 (Finder)
-    return render_template('page6.html', found_feeds=found_feeds, search_url=url, error=error, categories=user_data['categories'])
+    # Render PAGE5.HTML (Finder)
+    return render_template('page5.html', found_feeds=found_feeds, search_url=url, error=error, categories=user_data['categories'])
 
 @app.route('/help')
 def help_page(): 
-    # Render Page 7 (Help)
-    return render_template('page7.html')
+    # Render PAGE6.HTML (Help)
+    return render_template('page6.html')
 
 @app.route('/about')
 def about(): 
-    # Render Page 8 (About)
-    return render_template('page8.html')
+    # Render PAGE7.HTML (About)
+    return render_template('page7.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
